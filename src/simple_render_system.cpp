aff02a9bf94c19782cdf297035de60a81b445eae
matthewparnham <parnham@chapman.edu>
2021-08-16 16:53:55 -0700

    Added camera viewing transformation

--- src/simple_render_system.cpp
+++ src/simple_render_system.cpp
@@ -62,14 +62,15 @@ namespace lve {
 	void SimpleRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer, std::vector<LveGameObject> &gameObjects, LveCamera &camera) {
 		lvePipeline->bind(commandBuffer);
 
+		auto projectionView = camera.getProjection() * camera.getView();
+
 		for(auto& obj: gameObjects) {
 			obj.transform.rotation.y = glm::mod(obj.transform.rotation.y +0.0001f, glm::two_pi<float>());
 			obj.transform.rotation.x = glm::mod(obj.transform.rotation.x +0.00005f, glm::two_pi<float>());
-			obj.transform.translation.z += 0.00001f;
 
 			SimplePushConstantData push{};
 			push.color = obj.color;
-			push.transform = camera.getProjection() * obj.transform.mat4();
+			push.transform = projectionView * obj.transform.mat4();
 
 			vkCmdPushConstants(commandBuffer, pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(SimplePushConstantData), &push);
 			obj.model->bind(commandBuffer);
