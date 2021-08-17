a74a5818b48e301d7ca8b62140917ce354e12a47
matthewparnham <parnham@chapman.edu>
2021-08-17 16:30:18 -0700

    Added tinyobjloader and 3d model loading

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 include .env
-CFLAGS = -std=c++17 -I. -I$(VULKAN_SDK_PATH)\Include -I$(GLFW_PATH)\include -I$(GLM_PATH)
+CFLAGS = -std=c++17 -I. -I$(VULKAN_SDK_PATH)\Include -I$(GLFW_PATH)\include -I$(GLM_PATH) -I$(TINYOBJ_PATH)
 LDFLAGS = -L$(GLFW_PATH)\lib-mingw-w64 -L$(VULKAN_SDK_PATH)/Lib -lglfw3dll -lvulkan-1
 
 # target = a.out - Compiles all .cpp files in src into a.out
--- src/lve_model.hpp
+++ src/lve_model.hpp
@@ -7,6 +7,8 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 
+//std
+#include <memory>
 #include <vector>
 
 namespace lve {
@@ -16,14 +18,22 @@ namespace lve {
             struct Vertex {
                 glm::vec3 position;
                 glm::vec3 color;
+                glm::vec3 normal{};
+                glm::vec2 uv{};
 
                 static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
                 static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+
+                bool operator==(const Vertex &other) const {
+                    return position == other.position && color == other.color && normal == other.normal && uv == other.uv;
+                }
             };
 
             struct Builder {
                 std::vector<Vertex> vertices{};
                 std::vector<uint32_t> indices{};
+
+                void loadModel(const std::string &filepath);
             };
 
             LveModel(LveDevice &device, const LveModel::Builder &builder);
@@ -31,6 +41,8 @@ namespace lve {
 
             LveModel(const LveModel &) = delete;
             LveModel &operator=(const LveModel &) = delete;
+
+            static std::unique_ptr<LveModel> createModelFromFile(LveDevice &device, const std::string &filepath);
 
             void bind(VkCommandBuffer commandBuffer);
             void draw(VkCommandBuffer commandBuffer);
